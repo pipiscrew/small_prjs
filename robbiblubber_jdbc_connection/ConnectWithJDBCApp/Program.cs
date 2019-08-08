6379d8ad9f7ab61cb1f8fb023c5699323ea4d657
unknown <pipiscrew@gmail.com>
2019-08-08 21:51:52 +0300

    add Robbiblubber

---
+++ robbiblubber_jdbc_connection/ConnectWithJDBCApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using Robbiblubber.Extend.SQL.JDBC.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectWithJDBCApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sqlite_filepath = AppDomain.CurrentDomain.BaseDirectory + "\\test.sqlite";
+
+            //Directory.CreateDirectory("c:\\test");
+            JdbcConnection cn = new JdbcConnection("jdbc:sqlite:" + sqlite_filepath, "org.sqlite.JDBC");
+
+            //if doesnt exist, will be created
+            cn.Open();
+
+            JdbcCommand cmd;
+            JdbcReader re = null;
+
+            try
+            {
+                //check if table exists
+                cmd = cn.CreateCommand("SELECT * FROM USERS");
+                re = cmd.ExecuteReader();
+            }
+            catch { }
+
+            if (re == null || !re.Read())
+            {
+                //create the USERS table
+                cmd = cn.CreateCommand("CREATE TABLE [users] (user_id INTEGER PRIMARY KEY, user_mail TEXT, user_password TEXT, user_level INTEGER)");
+                cmd.ExecuteNonQuery();
+            }
+
+            //insert one record
+            cmd = cn.CreateCommand("INSERT INTO [users] (user_mail, user_password, user_level) VALUES ('test@test.com', '123456', 1)");
+            cmd.ExecuteNonQuery();
+
+
+            //select the table
+            cmd = cn.CreateCommand("SELECT * FROM USERS");
+            re = cmd.ExecuteReader();
+
+            //select the rows
+            while (re.Read())
+            {
+                for (int i = 0; i < re.FieldCount; i++)
+                {
+                    Console.WriteLine(re.GetString(i) + "\r\n");
+                }
+
+            }
+            re.Close();
+            cn.Close();
+
+            Console.WriteLine("Press enter to close.");
+            Console.ReadLine();
+        }
+    }
+}
