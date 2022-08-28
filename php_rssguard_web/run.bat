38a0f813fc4371822c587e1091b35510e249a9e7
pipiscrew <pipiscrew@gmail.com>
2022-08-28 23:48:21 +0300

    Create run.bat

---
+++ php_rssguard_web/run.bat
@@ -0,0 +1,8 @@
+::Add this batch file and php near 'C:\Program Files\rssguard\data\database\local\' folder
+
+::start the browser to locahost - https://stackoverflow.com/a/154090
+start "" "C:\Program Files\+browsers\icecat\icecat.exe" http://localhost:8000
+
+::start the php builtin developer server
+D:\phpFE\php\php.exe -S localhost:8000 "C:\Program Files\rssguard\data\database\local\feed.php"
+
